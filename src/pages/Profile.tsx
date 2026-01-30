b62a7b5e01ca86a96d930acf5dbc3b7dfa4e1a16
Azamat493 <zukurboyev@gmail.com>
2026-01-30 17:00:52 +0500

    Описание изменений

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Cookies from "js-cookie";
 import { message, Upload, Button, Modal } from "antd";
